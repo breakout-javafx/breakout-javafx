8c40d69b6c1f3ac67ecc63b6d0a7e5ea4364a015
JavierDomi <javier.dominguez.ib@gmail.com>
2025-05-05 17:34:48 +0200

    Carga texturas de ball y paddle desde .properties

--- breakout-game/src/main/java/com/breakout/entities/paddle/Paddle.java
+++ breakout-game/src/main/java/com/breakout/entities/paddle/Paddle.java
@@ -2,8 +2,12 @@ package com.breakout.entities.paddle;
 
 import com.breakout.config.ConfigLoader;
 import com.breakout.core.GameApp;
+import com.breakout.entities.brick.decorator.StandardBrick;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+
+import java.util.Objects;
 
 public class Paddle {
     private double x;
@@ -11,6 +15,19 @@ public class Paddle {
     private final double width;
     private final double height;
     private final double speed;
+
+    private static final Image TEXTURE;
+
+    static {
+        TEXTURE = new Image(
+                Objects.requireNonNull(StandardBrick.class.getResourceAsStream(
+                        ConfigLoader.getInstance().get("paddle.texture"))),
+                0, 0, false, false
+        );
+        if (TEXTURE.isError()) {
+            System.err.println("No se pudo cargar la textura de la bola.");
+        }
+    }
 
     public Paddle(double initialX, double initialY) {
         // Leer las configuraciones desde el archivo .properties
@@ -33,8 +50,12 @@ public class Paddle {
     }
 
     public void render(GraphicsContext gc) {
-        gc.setFill(Color.BLUE);
-        gc.fillRect(x, y, width, height);
+        if (TEXTURE != null && !TEXTURE.isError()) {
+            gc.drawImage(TEXTURE, x, y, width, height);
+        } else {
+            gc.setFill(Color.BLUE);
+            gc.fillRect(x, y, width, height);
+        }
     }
 
     public double getX() { return x; }
