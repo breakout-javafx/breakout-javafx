32f378547fbb5022b91fdff41a1d406713141255
JavierDomi <javier.dominguez.ib@gmail.com>
2025-05-05 16:41:04 +0200

    Cambio texura perdiad por perdida de vida. Cambio texturas y correción de nombres

--- breakout-game/src/main/java/com/breakout/entities/brick/decorator/GlowingBrickDecorator.java
+++ breakout-game/src/main/java/com/breakout/entities/brick/decorator/GlowingBrickDecorator.java
@@ -4,35 +4,42 @@ import com.breakout.config.ConfigLoader;
 import com.breakout.entities.brick.AbstractBrick;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
 
 import java.util.Objects;
 
 public class GlowingBrickDecorator extends BrickDecorator {
 
     private static final Image TEXTURE;
+    private final int maxHealth;
 
     static {
-        TEXTURE = new Image(Objects.requireNonNull(GlowingBrickDecorator
-                .class.getResourceAsStream(ConfigLoader.getInstance().get("ball.decorator.glowing.texture"))));
+        TEXTURE = new Image(
+                Objects.requireNonNull(StandardBrick.class.getResourceAsStream(
+                        ConfigLoader.getInstance().get("brick.decorator.glowing.texture"))),
+                0, 0, false, false
+        );
         if (TEXTURE.isError()) {
-            System.err.println("No se pudo cargar la textura de GlowingBrickDecorator.");
+            System.err.println("No se pudo cargar la textura de StandardBrick.");
         }
     }
 
+
+
     public GlowingBrickDecorator(AbstractBrick decoratedBrick) {
         super(decoratedBrick);
-        this.score = ConfigLoader.getInstance().getInt("ball.decorator.glowing.score");
-        this.health = ConfigLoader.getInstance().getInt("ball.decorator.glowing.health");
+        this.score = ConfigLoader.getInstance().getInt("brick.decorator.glowing.score");
+        this.health = ConfigLoader.getInstance().getInt("brick.decorator.glowing.health");
+        this.maxHealth = this.health;
     }
 
     @Override
     public void render(GraphicsContext gc) {
-        super.render(gc); // Render base
-        gc.drawImage(TEXTURE, x, y, width, height); // Textura encima
-        gc.setStroke(Color.YELLOW);
-        gc.setLineWidth(3);
-        gc.strokeRect(x, y, width, height);
+        super.render(gc); // Render base brick
+
+        double opacity = Math.max(0.2, (double) health / maxHealth);
+        gc.setGlobalAlpha(opacity);
+        gc.drawImage(TEXTURE, x, y, width, height);
+        gc.setGlobalAlpha(1.0);
     }
 
     @Override
